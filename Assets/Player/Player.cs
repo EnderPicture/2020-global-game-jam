b3d6aa10f82c4b9298ef52ce03567807cc5c76b9
Donny Wu <enderpicture@gmail.com>
2020-02-01 03:48:48 -0800

    player movement system

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -6,23 +6,58 @@ public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public int speed = 1;
+    public float burstSpeed = 0;
+    public float acc = 0;
+    public float maxSpeed = 0;
+
+    [Range(0.0f, 1.0f)]
+    public float drag = 0;
+
+
+    int lastDirection;
+
+    float speed = 0;
     Rigidbody rigidbody;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        int direction = 0;
 
-    }
-    private void FixedUpdate()
-    {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal") * speed;
-        rigidbody.AddForce(new Vector3(horizontal, 0, 0));
+
+        if (horizontal > 0)
+        {
+            direction = 1;
+        }
+        else if (horizontal < 0)
+        {
+            direction = -1;
+        }
+
+
+        if (direction != lastDirection && direction != 0)
+        {
+            speed = burstSpeed * direction;
+        }
+        else if (direction != 0)
+        {
+            speed += acc * direction;
+        }
+        else
+        {
+            speed -= speed * drag;
+        }
+
+        lastDirection = direction;
+
+        speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
+        Vector3 velocity = rigidbody.velocity;
+        velocity.x = speed;
+        rigidbody.velocity = velocity;
 
     }
 }
