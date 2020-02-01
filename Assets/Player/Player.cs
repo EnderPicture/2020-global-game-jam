67e8757d9a3e04df564825e7ed467aac1b943c73
Donny Wu <enderpicture@gmail.com>
2020-02-01 00:02:18 -0800

    player init

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int speed = 1;
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        rigidbody.AddForce(new Vector3(horizontal, 0, 0));
+
+    }
+}
