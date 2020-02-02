c593d4a3200f9a7deb870933c27b555ddb048978
Donny Wu <enderpicture@gmail.com>
2020-02-01 19:50:45 -0800

    merged master into this

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -10,27 +10,32 @@ public class SpawnEnemy : MonoBehaviour
     void Start()
     {
 
-        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-    timer += Time.deltaTime;
-        if (timer >= 2){
+        timer += Time.deltaTime;
+        if (timer >= 2)
+        {
             float randomspawn;
-            randomspawn = Random.Range(0,3);
+            randomspawn = Random.Range(0, 3);
 
-            if (randomspawn == 0){
-            Spawn0();
+            if (randomspawn == 0)
+            {
+                Spawn0();
             }
-            if (randomspawn == 1){
-            Spawn1();
+            if (randomspawn == 1)
+            {
+                Spawn1();
             }
-            if (randomspawn == 2){
-            Spawn2();
+            if (randomspawn == 2)
+            {
+                Spawn2();
             }
-            if (randomspawn == 3){
-            Spawn3();
+            if (randomspawn == 3)
+            {
+                Spawn3();
             }
 
 
@@ -41,22 +46,22 @@ public class SpawnEnemy : MonoBehaviour
 
 
     }
-        void Spawn0 ()
-        {
-            //float extraspawn;
-            //extraspawn = Random.range(0,3);
-         //GameObject = Instantiate(Enemy, transform.position, transform.rotation);
-        }
-        void Spawn1 ()
-        {
-         //GameObject = Instantiate(Enemy, transform.position, transform.rotation);
-        }
-        void Spawn2 ()
-        {
-         //GameObject = Instantiate(Enemy, transform.position, transform.rotation);
-        }
-        void Spawn3 ()
-        {
+    void Spawn0()
+    {
+        //float extraspawn;
+        //extraspawn = Random.range(0,3);
+        //GameObject = Instantiate(Enemy, transform.position, transform.rotation);
+    }
+    void Spawn1()
+    {
+        //GameObject = Instantiate(Enemy, transform.position, transform.rotation);
+    }
+    void Spawn2()
+    {
+        //GameObject = Instantiate(Enemy, transform.position, transform.rotation);
+    }
+    void Spawn3()
+    {
         // GameObject = Instantiate(Enemy, transform.position, transform.rotation);
-        }
+    }
 }
