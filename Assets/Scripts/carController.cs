463fbb787b68a87b56fcb0f3b93bf206996f9409
kbramk <kbramk@gmail.com>
2020-02-02 08:20:16 -0800

    changed player/enemy/camera/car prefabs and main scene

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -18,7 +18,7 @@ public class carController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = 20;
+        currentHealth = 10;
         tryMakeInvincible = false;
         isInvincible = false;
         hp.UpdateValues(currentHealth);
