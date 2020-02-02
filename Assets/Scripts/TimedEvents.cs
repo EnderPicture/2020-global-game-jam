9254eadb00e953d4470bc62954caf13ae91db5c9
kbramk <kbramk@gmail.com>
2020-02-02 09:59:43 -0800

    cluster spawn

--- Assets/Scripts/TimedEvents.cs
+++ Assets/Scripts/TimedEvents.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class TimedEvents : MonoBehaviour
 {
     public carController Car;
+    public GameObject Enemy;
     public GameObject RedEnemy;
     public SpawnEnemy spawnEnemy;
+    bool Event0 = false;
     bool Event1 = false;
     bool Event2 = false;
     bool Event3 = false;
@@ -13,11 +15,20 @@ public class TimedEvents : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Car.currentHealth >= 20 && Event0 == false) Spawn0();
         if (Car.currentHealth >= 25 && Event1 == false) Spawn1();
         if (Car.currentHealth >= 50 && Event2 == false) Spawn2();
         if (Car.currentHealth >= 75 && Event3 == false) Spawn3();
 
+        void Spawn0 ()
+        {
+                int numEnemies = 50;
+        for (int i = 0; i < numEnemies; i++)
+            {
+            spawnEnemy.spawnXPatternObject(true,false,true,false, Enemy);
+            Event0 = true;
+            }
+        }
         void Spawn1 ()
         {
             spawnEnemy.spawnXPatternObject(true,false,true,false, RedEnemy);
